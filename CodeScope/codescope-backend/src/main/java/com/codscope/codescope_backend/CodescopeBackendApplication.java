15a2e06d11bf100977d2e5bdf718fb286a70a837
NazeefRizna <nazeefrizna@gmail.com>
2026-07-18 19:14:05 +0530

    Intialization

---
+++ CodeScope/codescope-backend/src/main/java/com/codscope/codescope_backend/CodescopeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codscope.codescope_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodescopeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodescopeBackendApplication.class, args);
+	}
+
+}
