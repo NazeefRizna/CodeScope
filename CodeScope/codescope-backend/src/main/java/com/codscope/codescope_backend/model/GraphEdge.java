70083ca906bd836e8b2dcd3bff4920e7fdd98d7d
NazeefRizna <nazeefrizna@gmail.com>
2026-08-01 18:07:29 +0530

    Create GraphEdge.java Response Model

---
+++ CodeScope/codescope-backend/src/main/java/com/codscope/codescope_backend/model/GraphEdge.java
@@ -0,0 +1,20 @@
+package com.codscope.codescope_backend.model;
+
+public class GraphEdge {
+
+    private final String source;
+    private final String target;
+
+    public GraphEdge(String source, String target) {
+        this.source = source;
+        this.target = target;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+}
