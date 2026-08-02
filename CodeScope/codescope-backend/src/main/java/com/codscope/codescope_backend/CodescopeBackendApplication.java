71d592c2870e0995a77f252f8dcae3718fa4dc6c
NazeefRizna <nazeefrizna@gmail.com>
2026-08-02 19:40:17 +0530

    Main updated to run as a SpingBootApplication

--- CodeScope/codescope-backend/src/main/java/com/codscope/codescope_backend/CodescopeBackendApplication.java
+++ CodeScope/codescope-backend/src/main/java/com/codscope/codescope_backend/CodescopeBackendApplication.java
@@ -1,63 +1,27 @@
 package com.codscope.codescope_backend;
 
+import com.codscope.codescope_backend.heap.HeapNode;
 import com.codscope.codescope_backend.model.ClassInfo;
-import com.codscope.codescope_backend.scanner.JavaCodeScanner;
+import com.codscope.codescope_backend.service.CodeScopeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-import com.codscope.codescope_backend.graph.Graph;
-
 @SpringBootApplication
-public class CodescopeBackendApplication implements CommandLineRunner {
+public class CodescopeBackendApplication
+        implements CommandLineRunner {
+    
 
     public static void main(String[] args) {
-        SpringApplication.run(CodescopeBackendApplication.class, args);
+
+        SpringApplication.run(
+                CodescopeBackendApplication.class,
+                args
+        );
     }
 
     @Override
     public void run(String... args) {
 
-        String projectPath = "C:/Users/nazee/Desktop/CodeScope 2";
-
-        JavaCodeScanner scanner = new JavaCodeScanner();
-
-        System.out.println("Scanning project: " + projectPath);
-
-        List<ClassInfo> classes = scanner.scanProject(projectPath);
-
-        Graph graph = new Graph();
-
-        System.out.println(" \n---CLASSES FOUND---\n");
-
-        if (classes.isEmpty()) {
-            System.out.println("No Java classes were found.");
-            return;
-        }
-
-        for (ClassInfo classInfo : classes) {
-
-            System.out.println("Class      : " + classInfo.getClassName());
-            System.out.println("Package    : " + classInfo.getPackageName());
-            System.out.println("File Path  : " + classInfo.getFilePath());
-
-            System.out.println("-----------------------------------");
-        }
-
-        System.out.println("Total Classes Found : " + classes.size());
-
-        for (ClassInfo classInfo : classes) {
-            graph.addClass(classInfo);
-        }
-
-        scanner.buildDependencies(
-                classes,
-                graph
-        );
-
-        graph.displayGraph();
-        graph.displayDependencyCounts();
-        graph.depthFirstSearch("Main");
     }
 }
